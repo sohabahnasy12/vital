cb6024cc739ca1198b46e99c6a2644900d0fa628
sohabahnasy12 <154708183+sohabahnasy12@users.noreply.github.com>
2023-12-24 00:49:27 +0200

    Add files via upload

---
+++ creat.php
@@ -0,0 +1,31 @@
+<?php
+$firstname= $_REQUEST['first name'];
+$lasttname= $_REQUEST['last name'];
+$email= $_REQUEST['email'];
+$password= $_REQUEST['password'];
+$gender= $_REQUEST['gender'];
+$days= $_REQUEST['Days'];
+$Monthes= $_REQUEST['Monthes'];
+$years= $_REQUEST['Years'];
+if(isset($_POST['submit']))
+{
+
+$host="localhost";
+$user = "soha";
+$password="";
+$db_name="project";
+$conn = mysqli_connect($host,$user,$password,$db_name);
+
+$insert= "insert into creat values('$firstname','$lasttname','$email','$password','$gender','$days','$Monthes','$years') ";
+
+mysqli_query($conn , $insert);
+
+if($conn){
+    echo("<h1 style='color:green;'> your rrgestration is done</h1>");
+}
+else{
+    echo("<h1 style='color:green;'> your rrgestration is failed</h1>");
+}
+}
+
+?>
